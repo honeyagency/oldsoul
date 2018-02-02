7ae7e541621e93d0fa3d8410c942e475f01999c3
joshre <joshre@me.com>
2018-02-01 16:22:03 -0800

    Styles

--- woocommerce/emails/email-styles.php
+++ woocommerce/emails/email-styles.php
@@ -97,6 +97,10 @@ $text_lighter_20 = wc_hex_lighter( $text, 20 );
         display: block;
         width: 100%;
     }
+    #addresses > tr > td {
+display:block;
+width:100% !important;
+}
 }
 
 #template_header {
