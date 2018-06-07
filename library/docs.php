9ab2ab8c0e1fdecca053620890425ccef65d3365
joshre <joshre@me.com>
2018-06-07 16:56:42 -0700

    Docs

---
+++ library/docs.php
@@ -0,0 +1,29 @@
+<style type="text/css" media="screen">li{ padding-left: 20px;}.docitem{margin-bottom: 10px;}</style>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#wordpress-overview">WordPress Overview</a></div>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#logging-in">Logging In</a></div>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#media-library">Media Library</a></div>
+	<ul>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-a-media-item">Adding a Media Item</a></li>
+</ul>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-editing-a-page">Adding/Editing a Page</a></div>
+	<ul>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-a-new-page">Adding A New Page</a></li>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#content">Content</a></li>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#published-draft-schedulded-private">Published / Draft / Scheduled / Private</a></li>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#editing-a-page">Editing a Page</a></li>
+</ul>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-featured-images">Adding a Featured Image</a></div>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-a-product">Adding a Product</a></div>
+<div class="docitem"><a href="coffee-details">Coffee Details</a></div>
+	<ul>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#feature">Featured Product Info</a></li>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#product-fields">Product Fields</a></li>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-variants">Adding Variants</a></li>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#featuring-products">Featuring Products (Shop)</a></li>
+</ul>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#handling-orders">Handling Orders</a></div>
+	<ul>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#purchasing-labels">Purchasing Labels</a></li>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#issuing-refunds">Issuing Refunds</a></li>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#reports">Reports</a><br/></li>
+</ul>
